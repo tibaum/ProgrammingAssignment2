13d52e7ec9dc1fd97f2da8d570592a36d2ae902f
tibaum <tibaum3@gmail.com>
2016-06-16 22:12:42 +0200

    Implementation of makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions makeCacheMatrix and cacheSolve work work together to compute
+## and cache the inverse of a matrix. Function calls for repeated computations
+## return the cached inverse instead of computing it again.
 
-## Write a short comment describing this function
 
+## This function creates an object to store a matrix and cache its inverse once
+## that it is computated by the function cacheSolve.
+## The object created by makeCacheMatrix provides the following functions:
+##      get(): returns the matrix itself
+##      set(new_matrix): replaces the matrix and deletes the cached inverse
+##      get_inverse(): returnes the cached inverse if present, else NULL
+##      set_inverse(inverse_matrix): puts the inverse in the cache
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(new_matrix) {
+        x <<- new_matrix
+        inverse <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    set_inverse <- function(inverse_matrix) {
+        inverse <<- inverse_matrix
+    }
+    get_inverse <- function() {
+        inverse
+    }
+    list(set = set,
+         get = get,
+         set_inverse = set_inverse,
+         get_inverse = get_inverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of a matrix that was created by the
+## function makeCacheMatrix and stores it in the cache. If the cache already
+## contains the inverse then it is retrieved from there.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$get_inverse()
+    if (!is.null(inverse)) {
+        message("getting cached inverse")
+        return(inverse)
+    }
+    inverse <- solve(x$get())
+    x$set_inverse(inverse)
+    inverse
 }
